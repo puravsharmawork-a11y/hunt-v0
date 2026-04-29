b5323dfe122f4d623b9f2d86dee35e7a8967a6d1
puravsharmawork-a11y <puravsharma.work@gmail.com>
2026-04-29 06:58:34 +0530

    Delete src/components/StudentDashboard directory

--- src/components/StudentDashboard/theme/applyTokens.js
+++
@@ -1,7 +0,0 @@
-// src/components/StudentDashboard/theme/applyTokens.js
-import { tokens } from './tokens';
-
-export function applyTokens(theme) {
-  const root = document.documentElement;
-  Object.entries(tokens[theme]).forEach(([k, v]) => root.style.setProperty(k, v));
-}
